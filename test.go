771744d367e5a8fb6a8be00ab05dd7a2149ef94e
JevantJedidia <jevantjedidia@gmail.com>
2022-04-22 17:41:24 +0700

    boyerMoore

--- test.go
+++ test.go
@@ -1,23 +1,31 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"regexp"
-)
+// import (
+// 	"database/sql"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"regexp"
 
-func checkDNAInput(isiFile string) bool {
-	var re = regexp.MustCompile(`^[AGCT]+$`)
-	return re.MatchString(isiFile)
-}
+// 	_ "github.com/go-sql-driver/mysql"
+// )
 
-func main() {
-	var fileName string
-	fmt.Scanf("%s", &fileName)
-	body, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		log.Fatalf("unable to read file: %v", err)
-	}
-	fmt.Println(checkDNAInput(string(body)))
-}
+// func checkDNAInput(isiFile string) bool {
+// 	var re = regexp.MustCompile(`^[AGCT]+$`)
+// 	return re.MatchString(isiFile)
+// }
+
+// func main() {
+// 	var fileName string
+// 	fmt.Scanf("%s", &fileName)
+// 	body, err := ioutil.ReadFile(fileName)
+// 	if err != nil {
+// 		log.Fatalf("unable to read file: %v", err)
+// 	}
+// 	fmt.Println(checkDNAInput(string(body)))
+
+// 	db, err := sql.Open("mysql", "user:password@/dbname")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer db.Close()
