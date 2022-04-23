7d3f255361383f151395a3c7da6787e1bf9fc4ab
Gerald Abraham Sianturi <geraldabraham16@gmail.com>
2022-04-24 00:26:25 +0700

    Adding some component in page input disease and dna test

--- src/frontend/react-dna-tracker/src/pages/DNATest.js
+++ src/frontend/react-dna-tracker/src/pages/DNATest.js
@@ -2,6 +2,34 @@ export default function DNATest() {
     return (
         <section>
             <h1>DNA Test</h1>
+            <form onSubmit={handleSubmit} className="add-form">
+                <div className="form-control">
+                    <label>
+                        Patient name:
+                        <input type="text" placeholder="Input the patient name" />
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label>
+                        DNA Sequence:
+                        <input type="file"/>
+                    </label>
+                </div>
+                <div>
+                    <select>
+                        <option>Penyakit 1</option>
+                        <option>Penyakit 2</option>
+                        <option>Penyakit 3</option>
+                        <option>Penyakit 4</option>
+                    </select>
+                </div>
+
+                <input type='submit' value='Submit Test' className = 'btn btn-block'/>
+            </form>
         </section>
     )
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
 }
--- src/frontend/react-dna-tracker/src/pages/InputDisease.js
+++ src/frontend/react-dna-tracker/src/pages/InputDisease.js
@@ -1,7 +1,25 @@
 export default function InputDisease() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
         <section>
             <h1>Input Disease</h1>
+            <form onSubmit={handleSubmit} className="add-form">
+                <div className="form-control">
+                    <label>
+                        Disease name:
+                        <input type="text" placeholder="Input the disease name" />
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label>
+                        DNA Sequence
+                        <input type="file" name="name" />
+                    </label>
+                </div>
+                <input type='submit' value='Submit Disease' className = 'btn btn-block'/>
+            </form>
         </section>
     )
 }
