154d2fe3b777a2c1ed12ecebe51046f44b94aec6
JevantJedidia <jevantjedidia@gmail.com>
2022-04-24 18:27:32 +0700

    fix go workspace

--- src/backend/BM.go
+++ src/backend/BM.go
@@ -1,57 +1,29 @@
-package main
+package backend
 
-import (
-	"regexp"
-)
-
-func checkDNAInput(isiFile string) bool {
-	var re = regexp.MustCompile(`^[AGCT]+$`)
-	return re.MatchString(isiFile)
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-//ACGT
-func getL(str string) []int {
+//Mecari LastOccurence dari sebuah pattern
+func GetL(pattern string) []int {
 	var array []int = make([]int, 4)
 	for i := 0; i < 4; i++ {
 		array[i] = -1
 	}
 
-	for i := 0; i < len(str); i++ {
-		if str[i] == 'A' {
+	for i := 0; i < len(pattern); i++ {
+		if pattern[i] == 'A' {
 			array[0] = i
-		} else if str[i] == 'C' {
+		} else if pattern[i] == 'C' {
 			array[1] = i
-		} else if str[i] == 'G' {
+		} else if pattern[i] == 'G' {
 			array[2] = i
-		} else if str[i] == 'T' {
+		} else if pattern[i] == 'T' {
 			array[3] = i
 		}
 	}
 	return array
 }
 
-func changeToIndex(word string) int {
-	if word == "A" {
-		return 0
-	} else if word == "C" {
-		return 1
-	} else if word == "G" {
-		return 2
-	} else if word == "T" {
-		return 3
-	}
-	return -1
-}
-
-func boyerMoore(pattern, text string) int {
-	var lastOcc []int = getL(pattern)
+//String matching dengan Boyer-Moore
+func BoyerMoore(pattern, text string) int {
+	var lastOcc []int = GetL(pattern)
 	var n int = len(text)
 	var m int = len(pattern)
 	var i int = m - 1
@@ -65,21 +37,10 @@ func boyerMoore(pattern, text string) int {
 				j--
 			}
 		} else {
-			var occ int = lastOcc[changeToIndex(text[i:i+1])]
-			i = i + m - min(j, 1+occ)
+			var occ int = lastOcc[ChangeToIndex(text[i:i+1])]
+			i = i + m - Min(j, 1+occ)
 			j = m - 1
 		}
 	}
 	return -1
-}
-
-func main() {
-	var text string = "AGCTAGCTCGTCGCTAGCTGTCGAGCTCGATCGCTGATAG"
-	var pattern string = "TCGA"
-	//fmt.Scanf("%s", &fileName)
-	if checkDNAInput(text) {
-		print(boyerMoore(pattern, text))
-	} else {
-		print("Input salah")
-	}
 }
--- src/backend/util.go
+++ src/backend/util.go
@@ -1,15 +1,31 @@
-package main
+package backend
 
 import "regexp"
 
-func checkDNAInput(isiFile string) bool {
+// Sanitasi DNA dengan regex
+func CheckDNAInput(isiFile string) bool {
 	var re = regexp.MustCompile(`^[AGCT]+$`)
 	return re.MatchString(isiFile)
 }
 
-func min(a, b int) int {
+// Cari nilai minimal dari 2 integer
+func Min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
+}
+
+//Mengubah huruf A, C, G, dan T menjadi index
+func ChangeToIndex(word string) int {
+	if word == "A" {
+		return 0
+	} else if word == "C" {
+		return 1
+	} else if word == "G" {
+		return 2
+	} else if word == "T" {
+		return 3
+	}
+	return -1
 }
