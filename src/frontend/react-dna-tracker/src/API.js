37653d1328ad131dc331ba667210bb39f0c6a159
Gerald Abraham Sianturi <geraldabraham16@gmail.com>
2022-04-25 17:10:05 +0700

    Adding API connector to js (server not created yet)

---
+++ src/frontend/react-dna-tracker/src/API.js
@@ -0,0 +1,9 @@
+// ! Server belum di-create
+// export const apiURl = 'http://localhost:8081'
+
+// export const Endpoints = {
+// //   home: `${apiURl}/api/`,
+//   input_disease: `${apiURl}/api/input-disease`,
+//   dna_test: `${apiURl}/api/dna-test`,
+//   dna_test_result: `${apiURl}/api/dna-test-result`,
+// }
