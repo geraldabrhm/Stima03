d410ec7c99ee3ae3928d05e6a685539270e5bff6
atabiksf <13520159@std.stei.itb.ac.id>
2022-04-29 05:08:35 +0700

    add PredictionStatus to testDNACreateRequest

--- server/model/request/user.request.go
+++ server/model/request/user.request.go
@@ -1,12 +1,13 @@
 package request
 
 type DiseaseCreateRequest struct {
-	DiseaseName string	`json:"disease_name" validate:"required"`
-	DNASequence string	`json:"dna_sequence"`
+	DiseaseName string `json:"disease_name" validate:"required"`
+	DNASequence string `json:"dna_sequence"`
 }
 
 type TestDNACreateRequest struct {
-	PatientName      	string 		`json:"patient_name" validate:"required"`
-	IDDisease        	int    		`json:"id_disease" validate:"required"`
-	PatientDNA			string   	`json:"patient_dna"`
+	PatientName      string `json:"patient_name" validate:"required"`
+	IDDisease        int    `json:"id_disease" validate:"required"`
+	PatientDNA       string `json:"patient_dna"`
+	PredictionStatus bool   `json:"prediction_status"`
 }
